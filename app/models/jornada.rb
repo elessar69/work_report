a656e1feb9700e86cf6bb78063a671d5000bb815
Rubén Almirante <ruben.almirante@gmail.com>
2013-05-20 23:59:46 +0200

    Exportacion XLS (fix incidencias)

--- app/models/jornada.rb
+++ app/models/jornada.rb
@@ -1,6 +1,6 @@
 class Jornada < ActiveRecord::Base
   attr_accessible :horas, :incidencia, :observaciones, :fecha, :pilar, :usuario_id, :cliente_id
-  validates_presence_of :horas, :fecha, :pilar, :usuario_id, :cliente_id, :incidencia,
+  validates_presence_of :horas, :fecha, :pilar, :usuario_id, :cliente_id, :incidencia
   belongs_to :usuario
   belongs_to :cliente
 end
